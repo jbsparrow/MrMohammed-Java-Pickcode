f9262c59c96cde8d6c632d4faf259dcf535ef62d
Jacob <admin@script-ware.net>
2024-05-23 12:42:49 -0400

    Update Main.java

--- test/src/main/java/com/example/test/Main.java
+++ test/src/main/java/com/example/test/Main.java
@@ -2,6 +2,7 @@ package com.example.test;
 
 import java.io.*;
 import java.util.*;
+
 class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -9,17 +10,18 @@ class Main {
 		System.out.println("Encrpt/Decrypt? (E/d)");
 		String choiceString = sc.nextLine();
 		char choice = choiceString.trim().toLowerCase().charAt(0);
-		if (choice == 'e') {
+
+		if (choice == 'e') { // Encrypt
 			System.out.print("\nEnter the string to encrypt: ");
 			String input = sc.nextLine();
 			String encrypted = Cryptography.encrypt(input);
 			System.out.println("Encrypted string:\n\t" + encrypted);
-		} else if (choice == 'd') {
+		} else if (choice == 'd') { // Decrypt
 			System.out.print("\nEnter the string to decrypt: ");
 			String input = sc.nextLine();
 			String decrypted = Cryptography.decrypt(input);
 			System.out.println("Decrypted string:\n\t" + decrypted);
-		} else {
+		} else { // Invalid choice
 			System.out.println("Invalid choice");
 		}
 	}
