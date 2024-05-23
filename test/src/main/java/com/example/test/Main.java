e630ddbc074e1ab13e8ed822a13288bc151f7308
Jacob <admin@script-ware.net>
2024-05-23 13:23:54 -0400

    Update Main.java

--- test/src/main/java/com/example/test/Main.java
+++ test/src/main/java/com/example/test/Main.java
@@ -1,28 +1,31 @@
 package com.example.test;
 
-import java.io.*;
 import java.util.*;
+import java.util.regex.*;
+import java.io.*;
 
 class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Encrpt/Decrypt? (E/d)");
-		String choiceString = sc.nextLine();
-		char choice = choiceString.trim().toLowerCase().charAt(0);
+		System.out.println("Enter a string to search, then press RETURN, or type 'exit' to quit: ");
+		String text = sc.nextLine();
 
-		if (choice == 'e') { // Encrypt
-			System.out.print("\nEnter the string to encrypt: ");
-			String input = sc.nextLine();
-			String encrypted = Cryptography.encrypt(input);
-			System.out.println("Encrypted string:\n\t" + encrypted);
-		} else if (choice == 'd') { // Decrypt
-			System.out.print("\nEnter the string to decrypt: ");
-			String input = sc.nextLine();
-			String decrypted = Cryptography.decrypt(input);
-			System.out.println("Decrypted string:\n\t" + decrypted);
-		} else { // Invalid choice
-			System.out.println("Invalid choice");
+		if (text.toLowerCase().equals("exit")) {
+			System.out.println("Exiting...");
+			System.exit(1);
 		}
+
+		Pattern regex = Pattern.compile("([sS]\\s*[aA])", Pattern.MULTILINE);
+		Matcher matcher = regex.matcher(text);
+		StringBuilder matches = new StringBuilder();
+		int matchCount = 0;
+		while (matcher.find()) {
+			matchCount++;
+			matches.append("\t" + matchCount + ". " + matcher.group() + "\n");
+		}
+
+		System.out.println("\nFound " + matchCount + " matches from the string \"" + text + "\":");
+		System.out.println(matches.toString().stripTrailing());
 	}
 }
