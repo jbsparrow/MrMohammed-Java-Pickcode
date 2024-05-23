7c7a01ce66ab0dd25af2a8fc612b87317df4ddf6
Jacob <admin@script-ware.net>
2024-05-23 10:55:23 -0400

    Update Main.java
    Working on regex for addition and stuff

--- test/src/main/java/com/example/test/Main.java
+++ test/src/main/java/com/example/test/Main.java
@@ -1,25 +1,39 @@
 package com.example.test;
 
-import java.io.*;
-import java.util.*;
+import java.io.*; //Imports java library to allow keyboard inputs
+import java.util.*; //Imports java library to allow keyboard inputs
+import java.util.regex.Pattern; // Imports the regex pattern library to allow pattern compilation
 
-public class Main {
+class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); //Brings in a new scanner called "sc" which will allow a user to input data from their keyboard
+
+		System.out.print("Enter a math string\n"); //Prints text that asks the user for a math problem/math string
+		String s = sc.nextLine(); //States that the variable "s" will be what ever is inputted by the user
+
+		if (
+			(s.charAt(0) != '+') && (s.charAt(0) != '-')
+		) s = "+" + s; //An if statement looking at the first character of s and seeing if it is equal to a + or - sign //If there is no + or - sign at the begining of the string then it will add an addition symbol to the begining
+
+		Scanner sc1 = new Scanner(s); //Sets up a new scanner called "sc1" whihc will scan s
+		// sc1.useDelimiter("\\s+\\+\\s+|\\s+\\-\\s+"); //searches the input from sc1 for an addition symbol with any combination of spaces around it as well as a subtraction symbol with any combination of spaces around it
+		String regex = "((?:\\+|-)\\d+|\\d+)(?:\\s*(-|\\+))?";
+        sc1.findAll(Pattern.compile(regex)).forEach(m -> {
+            System.out.print(m.group(1));
+            if (m.group(2) != null) {
+                System.out.print(" " + m.group(2));
+            }
+            System.out.print(" ");
+        });
+
+		int sum = 0; //Sets the variable "sum" to equal 0
+
+		while (sc1.hasNextInt()) { // While loop that will run so long as there is an integer in the input
+			int n = sc1.nextInt(); //Sets the variable "n" to the next integer in the input
+			System.out.println("Added " + n + " to " + sum + " to get " + (sum + n));
+			sum += n; //Adds the next integer in the input to the sum
+		}
+
 		
-		System.out.print("Enter fuel efficiency (kmpl): ");
-		double kmpl = scanner.nextDouble();
-
-		Automobile myBmw = new Automobile(kmpl);
-
-		System.out.print("Enter how much fuel to put in the tank (litres): ");
-		double litres = scanner.nextDouble();
-		myBmw.fillUp(litres);
-
-		System.out.print("Enter how far you want to drive (kilometers): ");
-		double kilometers = scanner.nextDouble();
-		myBmw.takeTrip(kilometers);
-
-		System.out.println("Remaining fuel: " + ((double)Math.round(myBmw.reportFuel() * 100) / 100) + " litres. Fuel used for the trip: " + ((double)Math.round(myBmw.reportTripFuelUsage() * 100) / 100) + " litres.");
 	}
 }
