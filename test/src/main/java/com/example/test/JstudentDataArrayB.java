a49dfba7530db6eae216da370af675e7a773302f
Jacob <admin@script-ware.net>
2024-06-04 16:21:07 -0400

    Delete JstudentDataArrayB.java

--- test/src/main/java/com/example/test/JstudentDataArrayB.java
+++
@@ -1,7 +0,0 @@
-package com.example.test;
-
-public class JstudentDataArrayB {
-    public JstudentDataArrayB(Integer JstudentsB) {
-        String JstudentDataArrayB[] = new String[JstudentsB];
-    }
-}
