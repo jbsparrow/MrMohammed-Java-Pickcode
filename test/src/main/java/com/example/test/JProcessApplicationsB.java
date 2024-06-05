2d2f8d167441f2a373595a377604c6361f3189c7
Jacob <admin@script-ware.net>
2024-06-05 11:34:56 -0400

    Update JProcessApplicationsB.java
    Add acceptance/denial/waitlist/alternate offer letters.

--- test/src/main/java/com/example/test/JProcessApplicationsB.java
+++ test/src/main/java/com/example/test/JProcessApplicationsB.java
@@ -6,9 +6,26 @@ import java.util.*;
 
 public class JProcessApplicationsB {
     private static final String Joutput_fileB = "./JPCI.txt";
+    private static final String Juniversity_response_folderB = "./University Responses/";
+    private static final String[] Juniversity_programsB = {
+        "Computer Science (Honours)",
+        "Software Engineering (Co-op)",
+        "Artificial Intelligence and Machine Learning (Honours)",
+        "Cybersecurity (Honours)",
+        "Data Science and Analytics (Co-op)"
+    };
+    private static final String[] Jalternate_university_programsB = {
+        "Information Technology",
+        "Web Development and Design",
+        "Game Design and Development",
+        "Computer Networking",
+        "Database Management"
+    };
     private static Map<String, Integer> JstudentDataAveragesB = new Hashtable<String, Integer>();
+    private static Integer cutoff;
 
     public JProcessApplicationsB(Map<String, Integer[]> JstudentDataArrayB, Integer cutoff) throws IOException {
+        JProcessApplicationsB.cutoff = cutoff;
         FileWriter JfwB = new FileWriter(Joutput_fileB);
         PrintWriter JoutputB = new PrintWriter(JfwB);
 
@@ -35,5 +52,48 @@ public class JProcessApplicationsB {
 
         JoutputB.close();
         JfwB.close();
+
+        for (Map.Entry<String, Integer> JentryB : JstudentDataAveragesB.entrySet()) {
+            File Juniversity_response_fileB = new File(Juniversity_response_folderB + JentryB.getKey() + ".md");
+            FileWriter Jfw2B = new FileWriter(Juniversity_response_fileB);
+            PrintWriter Joutput2B = new PrintWriter(Jfw2B);
+            Random JrandomB = new Random();
+            String Juniversity_programB = Juniversity_programsB[JrandomB.nextInt(Juniversity_programsB.length)];
+            String Jalternate_university_programB = Jalternate_university_programsB[JrandomB.nextInt(Jalternate_university_programsB.length)];
+
+            Joutput2B.println("# Barr University\n");
+            Joutput2B.println("## Office of Admissions\n");
+            Joutput2B.println("Dear " + JentryB.getKey() + ",\n\n");
+
+
+            if (JentryB.getValue() >= cutoff + 10) {
+                // Accepted
+                Joutput2B.println("We are pleased to inform you that you have been accepted into the <b>" + Juniversity_programB + "</b> program at Barr University. Your academic achievements, extracurricular involvement, and passion for computer science have impressed our admissions committee.\n\n");
+                Joutput2B.println("We are excited to welcome you to our university community. Please review the enclosed materials for information on next steps, including important deadlines and orientation details.\n\n");
+                Joutput2B.println("Congratulations and welcome to Barr University!\n\n");
+                Joutput2B.println("Sincerely,\n\nJacob Barr\nDirector of Admissions\nBarr University");
+            } else if (JentryB.getValue() >= cutoff) {
+                // Waitlisted
+                Joutput2B.println("Thank you for your application to the <b>" + Juniversity_programB + "</b> program at Barr University. We are writing to inform you that your application has been placed on our waitlist.\n\n");
+                Joutput2B.println("This year's admissions process has been highly competitive, and while we cannot offer you admission at this moment, we see great potential in your application. Should a spot become available, we will notify you immediately.\n\n");
+                Joutput2B.println("We appreciate your patience and understanding.\n\n");
+                Joutput2B.println("Sincerely,\n\nJacob Barr\nDirector of Admissions\nBarr University");
+            } else if (JentryB.getValue() >= cutoff - 10) {
+                // Alternate Offer
+                Joutput2B.println("Thank you for your application to the <b>" + Juniversity_programB + "</b> program at Barr University. While we are unable to offer you a spot in this program at this time, we are pleased to extend an alternate offer for admission to the <b>" + Jalternate_university_programB + "</b> program.\n\n");
+                Joutput2B.println("We believe that your skills and interests align well with this program and that you will find it equally fulfilling. Please review the enclosed materials for more information on this opportunity.\n\n");
+                Joutput2B.println("We look forward to the possibility of welcoming you to Barr University.\n\n");
+                Joutput2B.println("Sincerely,\n\nJacob Barr\nDirector of Admissions\nBarr University");
+            } else {
+                // Rejected
+                Joutput2B.println("Thank you for your application to the <b>" + Juniversity_programB + "</b> program at Barr University. After careful consideration, we regret to inform you that we are unable to offer you admission at this time.\n\n");
+                Joutput2B.println("The admissions process is highly competitive, and we receive many applications from talented individuals. We encourage you to continue pursuing your academic and professional goals.\n\n");
+                Joutput2B.println("We wish you all the best in your future endeavors.\n\n");
+                Joutput2B.println("Sincerely,\n\nJacob Barr\nDirector of Admissions\nBarr University");
+            }
+
+            Joutput2B.close();
+            Jfw2B.close();
+        }
     }
 }
